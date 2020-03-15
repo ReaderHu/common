6fc1b09e92f8860b8e6194099607b876d32c06b9
wangh <592593695@qq.com>
2020-03-15 22:00:27 +0800

    up

---
+++ src/com/systop/strings/BeanUtil.java
@@ -0,0 +1,13 @@
+package com.systop.strings;
+/**
+ *
+ * @author 王浩
+ *
+ */
+public class BeanUtil {
+
+    public static  <T extends Object> T deepCopy(T entity){
+
+        return null;
+    }
+}
