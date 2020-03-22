6b3af8cce462c658b03698fcfe577f1dd96e5c17
wangh <592593695@qq.com>
2020-03-22 12:06:18 +0800

    DeepCopy

--- src/com/systop/strings/BeanUtil.java
+++ src/com/systop/strings/BeanUtil.java
@@ -1,4 +1,10 @@
 package com.systop.strings;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author 王浩
@@ -6,8 +12,36 @@ package com.systop.strings;
  */
 public class BeanUtil {
 
-    public static  <T extends Object> T deepCopy(T entity){
+    public static <T extends Object> T deepCopy(T entity) throws Exception {
 
-        return null;
+        ByteArrayOutputStream baos = null;
+        ObjectOutputStream oos = null;
+
+        ByteArrayInputStream bais = null;
+        ObjectInputStream ois = null;
+
+        Object o = null;
+        // 如果子类没有继承该接口，这一步会报错
+        try {
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(entity);
+            bais = new ByteArrayInputStream(baos.toByteArray());
+            ois = new ObjectInputStream(bais);
+
+            o = ois.readObject();
+            return (T) o;
+        } catch (Exception e) {
+            throw new Exception("对象中包含没有继承序列化的对象");
+        } finally {
+            try {
+                baos.close();
+                oos.close();
+                bais.close();
+                ois.close();
+            } catch (Exception e2) {
+                // 这里报错不需要处理
+            }
+        }
     }
 }
